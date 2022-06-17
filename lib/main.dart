5fd36702a105141596fd1cf02bd3fc581fadf741
KangSDhi <kangteknisi@gmail.com>
2022-06-17 18:50:45 +0700

    Flutter Basics [QUIZ APP]: (Quiz 1: Dart Fundamentals) Introducing Maps

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'Who\'s your favorite instrustor',
+        'answers': ['Sigit', 'Sigit', 'Sigit', 'Sigit']
+      }
     ];
     return MaterialApp(
       home: Scaffold(
